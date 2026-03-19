39a424f52698136f62d18fa103f80dedd210f2c9
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-19 09:16:27 +0500

    CoffeeFileAgent Initial Implementation Added.

---
+++ MyFirstChatUI/Agents/CoffeeFileAgent.cs
@@ -0,0 +1,19 @@
+﻿using Azure.AI.OpenAI;
+using MyFirstChatUI.Models;
+
+namespace MyFirstChatUI.Agents
+{
+	public class CoffeeFileAgent
+	{
+		private CoffeeFileAgent(AzureOpenAIClient azureOpenAIClient, CoffeeData coffeeDataService)
+		{
+
+		}
+
+		// Static factory method for async initialization
+		public static async Task<CoffeeFileAgent> CreateAsync(AzureOpenAIClient azureOpenAIClient, CoffeeData coffeeDataService)
+		{
+			return new CoffeeFileAgent(azureOpenAIClient, coffeeDataService);
+		}
+	}
+}
